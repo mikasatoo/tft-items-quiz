2ea4194c92db3ab8a25e8985baf8c4d38e63065f
Mika Sato <mika99@live.com.au>
2023-09-01 16:08:52 +1000

    replace icon image

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import { Quiz } from './components';
 import './App.css';
 import { Toaster } from 'react-hot-toast';
