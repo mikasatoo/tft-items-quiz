86176bf33f0fa77221224d3d9bd47be0dd48707d
Mika Sato <mika99@live.com.au>
2023-10-05 19:13:59 +1000

    refactor QuizQuestions to use the scraped data

--- src/components/QuizQuestions.tsx
+++ src/components/QuizQuestions.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useQuizContext } from '../context';
-import { itemImages, items } from '../helper/ItemsHelper';
+import { items } from '../helper/scrapedItemsHelper';
 import { toast } from 'react-hot-toast';
 import ProgressBar from '@ramonak/react-progress-bar';
 import styles from '../styles/quiz-styles.module.scss';
@@ -9,6 +9,13 @@ import styles from '../styles/quiz-styles.module.scss';
 type Question = {
     type: string
     text: string
+}
+
+// Define type for Option objects
+type Option = {
+    text: string
+    img1: string
+    img2: string
 }
 
 // Define questions array
@@ -32,7 +39,7 @@ const questions: Question[] = [
 ]
 
 // Function to shuffle the elements of an array randomly
-function shuffleArray(array: string[]) {
+function shuffleArray(array: Option[]) {
     const shuffledArray = [...array];
     for (let i = shuffledArray.length - 1; i > 0; i--) {
         const randomIndex = Math.floor(Math.random() * (i + 1));
@@ -49,17 +56,10 @@ export const QuizQuestions = () => {
     const [questionText, setQuestionText] = useState<string>('');
     const [questionContent, setQuestionContent] = useState<string>('');
     const [correctOption, setCorrectOption] = useState<string>('');
-    const [shuffledOptions, setShuffledOptions] = useState<string[]>([]);
+    const [shuffledOptions, setShuffledOptions] = useState<Option[]>([]);
     const [answer, setAnswer] = useState<string>('');
     const [result, setResult] = useState({ output: '', correctAnswer: '' });
-
     const [questionContentImgs, setQuestionContentImgs] = useState({ img1: '', img2: '' });
-    const [optionImgs, setOptionImgs] = useState({
-        option1Img1: '', option1Img2: '',
-        option2Img1: '', option2Img2: '',
-        option3Img1: '', option3Img2: '',
-        option4Img1: '', option4Img2: ''
-    });
 
     // Component to render the progress bar
     const QuizProgressBar = () => {
@@ -90,11 +90,11 @@ export const QuizQuestions = () => {
         // 3. Set the question content (including image/s) based on the question type
         if (questionType === 'chooseCombinedItem') {
             setQuestionContent(`${subject.baseItem1} + ${subject.baseItem2}`);
-            setQuestionContentImgs({ img1: itemImages[subject.baseItem1], img2: itemImages[subject.baseItem2] });
+            setQuestionContentImgs({ img1: subject.baseItem1Img, img2: subject.baseItem2Img });
         } else if (questionType === 'chooseBaseItems' || randomQuestion.type === 'chooseAbility') {
             setQuestionContent(subject.name);
             // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-            setQuestionContentImgs({ img1: itemImages[subject.name], img2: '' });
+            setQuestionContentImgs({ img1: subject.img, img2: '' });
         } else if (questionType === 'chooseItemFromAbility') {
             setQuestionContent(subject.ability);
             setQuestionContentImgs({ img1: '', img2: '' });
@@ -110,21 +110,45 @@ export const QuizQuestions = () => {
             }
         });
         
-        const options: string[] = [];
-        let possibleOptions: string[] = [];
+        const options: Option[] = [];
+        const possibleOptions: Option[] = [];
         
         if (questionType === 'chooseCombinedItem' || questionType === 'chooseItemFromAbility') {
             setCorrectOption(subject.name);
-            options.push(subject.name);
-            possibleOptions = otherItems.map(item => item.name);
+            options.push({ 
+                text: subject.name,
+                img1: subject.img,
+                img2: ''
+            });
+            otherItems.forEach((item) => possibleOptions.push({ 
+                text: item.name,
+                img1: item.img,
+                img2: ''
+            }));
         } else if (questionType === 'chooseBaseItems') {
             setCorrectOption(`${subject.baseItem1} + ${subject.baseItem2}`);
-            options.push(`${subject.baseItem1} + ${subject.baseItem2}`);
-            possibleOptions = otherItems.map(item => `${item.baseItem1} + ${item.baseItem2}`);
+            options.push({
+                text: `${subject.baseItem1} + ${subject.baseItem2}`,
+                img1: subject.baseItem1Img,
+                img2: subject.baseItem2Img
+            });
+            otherItems.forEach((item) => possibleOptions.push({
+                text: `${item.baseItem1} + ${item.baseItem2}`,
+                img1: item.baseItem1Img,
+                img2: item.baseItem2Img
+            }));
         } else if (questionType === 'chooseAbility') {
             setCorrectOption(subject.ability);
-            options.push(subject.ability);
-            possibleOptions = otherItems.map(item => item.ability);
+            options.push({
+                text: subject.ability,
+                img1: '',
+                img2: ''
+            });
+            otherItems.forEach((item) => possibleOptions.push({
+                text: item.ability,
+                img1: '',
+                img2: ''
+            }));
         }
 
         // loop through possibleOptions array to get the other three random options
@@ -138,46 +162,6 @@ export const QuizQuestions = () => {
         // shuffle the options array to randomize the order
         const newShuffledArray = shuffleArray(options);
         setShuffledOptions(newShuffledArray);
-
-        // set image/s for each option based on the question type
-        if (questionType === 'chooseCombinedItem' || questionType === 'chooseItemFromAbility') {
-            const option1 = newShuffledArray[0];
-            const option2 = newShuffledArray[1];
-            const option3 = newShuffledArray[2];
-            const option4 = newShuffledArray[3];
-            setOptionImgs({
-                option1Img1: itemImages[option1], option1Img2: '',
-                option2Img1: itemImages[option2], option2Img2: '',
-                option3Img1: itemImages[option3], option3Img2: '',
-                option4Img1: itemImages[option4], option4Img2: '',
-            });
-        } else if (questionType === 'chooseBaseItems') {
-            const option1Array = newShuffledArray[0].split(' + ');
-            const option1Part1 = option1Array[0];
-            const option1Part2 = option1Array[option1Array.length - 1];
-            const option2Array = newShuffledArray[1].split(' + ');
-            const option2Part1 = option2Array[0];
-            const option2Part2 = option2Array[option2Array.length - 1];
-            const option3Array = newShuffledArray[2].split(' + ');
-            const option3Part1 = option3Array[0];
-            const option3Part2 = option3Array[option3Array.length - 1];
-            const option4Array = newShuffledArray[3].split(' + ');
-            const option4Part1 = option4Array[0];
-            const option4Part2 = option4Array[option4Array.length - 1];
-            setOptionImgs({
-                option1Img1: itemImages[option1Part1], option1Img2: itemImages[option1Part2],
-                option2Img1: itemImages[option2Part1], option2Img2: itemImages[option2Part2],
-                option3Img1: itemImages[option3Part1], option3Img2: itemImages[option3Part2],
-                option4Img1: itemImages[option4Part1], option4Img2: itemImages[option4Part2],
-            });
-        } else if (questionType === 'chooseAbility') {
-            setOptionImgs({
-                option1Img1: '', option1Img2: '',
-                option2Img1: '', option2Img2: '',
-                option3Img1: '', option3Img2: '',
-                option4Img1: '', option4Img2: '',
-            });
-        }
     }
 
     // Function to handle selecting an answer (before submitting)
@@ -217,58 +201,34 @@ export const QuizQuestions = () => {
     }
 
     // Function to render each option button
-    function createOptionButton(optionNum: number, optionValue: string) {   
+    function createOptionButton(index: number, text: string, img1: string, img2: string) {   
         let optionClassName: string;
-        let optionImg1: string;
-        let optionImg2: string;
-        let optionLetter: string;
-
-        if (optionValue === correctOption) {
+        if (text === correctOption) {
             optionClassName = 'correctOption';
-        } else if (optionValue === answer) {
+        } else if (text === answer) {
             optionClassName = 'incorrectOption';
         } else {
             optionClassName = 'option';
         }
-
-        if (optionNum === 1) {
-            optionImg1 = optionImgs.option1Img1;
-            optionImg2 = optionImgs.option1Img2;
-            optionLetter = 'a';
-        } else if (optionNum === 2) {
-            optionImg1 = optionImgs.option2Img1;
-            optionImg2 = optionImgs.option2Img2;
-            optionLetter = 'b';
-        } else if (optionNum === 3) {
-            optionImg1 = optionImgs.option3Img1;
-            optionImg2 = optionImgs.option3Img2;
-            optionLetter = 'c';
-        } else if (optionNum === 4) {
-            optionImg1 = optionImgs.option4Img1;
-            optionImg2 = optionImgs.option4Img2;
-            optionLetter = 'd';
-        } else {
-            optionImg1 = '';
-            optionImg2 = '';
-            optionLetter = '';
-        }
+        
+        const optionLetter: string = String.fromCharCode(97 + index);
 
         return (
             <button
                 className={`${styles.optionBtn} ${styles[optionClassName]}`}
                 onClick={() =>
-                    (result.output === '' ? handleAnswerSelection(optionValue) : toast.error("You have already submitted your answer!"))
+                    (result.output === '' ? handleAnswerSelection(text) : toast.error("You have already submitted your answer!"))
                 }
             >
-                {optionLetter}. {optionValue}
-                {optionImg1 !== '' ? (
+                {optionLetter}. {text}
+                {img1 !== '' ? (
                     <div className={styles.optionImgs}>
-                        <img src={optionImg1}></img>
-                        {optionImg2 !== '' ? <img src={optionImg2}></img> : ''}
+                        <img src={img1}></img>
+                        {img2 !== '' ? <img src={img2}></img> : ''}
                     </div>
                 ) : ('')}
             </button>
-        )
+        );
     }
 
     // Run createQuestion() once per component mount
@@ -304,11 +264,9 @@ export const QuizQuestions = () => {
                 
                 <div className={result.output === '' ? styles.active : styles.inactive}>
                     <div className={styles.options}>
-                        {/* *** Make this into a component and pass props instead of using a function??? */}
-                        {createOptionButton(1, shuffledOptions[0])}
-                        {createOptionButton(2, shuffledOptions[1])}
-                        {createOptionButton(3, shuffledOptions[2])}
-                        {createOptionButton(4, shuffledOptions[3])}
+                        {shuffledOptions.map((option) => 
+                            createOptionButton(shuffledOptions.indexOf(option), option.text, option.img1, option.img2)
+                        )}
                     </div>
                 </div>
             </div>
